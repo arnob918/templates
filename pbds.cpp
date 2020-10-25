26a2a29b3f3ccee119f2759c9cd1a8c61c8fd438
arnob918 <64248431+arnob918@users.noreply.github.com>
2020-10-25 09:32:36 +0600

    Add files via upload

---
+++ pbds.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define D(x) cout << (#x) << " is " << x << endl;
+#define Q int t; scanf("%d", &t); for(int q=1; q<=t; q++)
+typedef long long int lli;
+typedef pair<int, int> pi;
+template <typename T>
+using o_set =
+    tree<T, null_type, greater<T>, rb_tree_tag, tree_order_statistics_node_update>;
+    
+
+//order_of_key (k) : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).(itterator)
+
+int main()
+{
+    
+ 
+    return 0;
+}
